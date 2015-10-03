562dbf9fce434475c8accbe846cf3b53eafbf0f6
cpintoval <cpintovalderrama@gmail.com>
2015-10-03 22:41:09 +0000

    Weather search working

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,9 +1,7 @@
 // App handlers
 var handlers = {
   autocomplete: function(query) {
-    console.log($('#place').val().length);
-    // console.log(availableTags.length);
-    if ($('#place').val().length > 0) {
+    if ($('#place').val().length > 4) {
       $.ajax ({
         url: "http://autocomplete.wunderground.com/aq",
         dataType: 'jsonp',
@@ -12,39 +10,47 @@ var handlers = {
           format: 'json',
           query: $('#place').val()
         },
-        success: handlers.processResults
+        success: handlers.processAutocomplete
       });
     }
     else {
-      var table = $('#places').find('tbody').empty();
+      // var table = $('#places').find('tbody').empty();
+      var $dataList = $('#json-datalist').empty();
+    }
+  },
+
+  processAutocomplete: function(data) {
+    var $dataList = $('#json-datalist').empty();
+    var i;
+    for (i in data.RESULTS) {
+      var place = data.RESULTS[i];
+      // var $option = $('<option>').text(place.name).data('l', place.l, 'll', place.ll);
+      var option = '<option id="' + place.l + '">' + place.name + '</option>'
+      // $option.appendTo($dataList);
+      $dataList.append(option);
     }
   },
 
   searchPlace: function(event) {
-    // event.preventDefault();
-    console.log("Query: " + $('#place').val());
-    $.ajax ({
-      url: "http://autocomplete.wunderground.com/aq",
-      dataType: 'jsonp',
-      jsonp: 'cb',
-      data: {
-        format: 'json',
-        query: $('#place').val()
-      },
-      success: handlers.processResults
-    });
+    event.preventDefault();
+    var value = $('#place').val();
+    var id = $('#json-datalist').find('option').filter(function() { return $.trim( $(this).text() ) === value; }).attr('id');
+    var link = "http://api.wunderground.com/api/ae1b201dc653f8ca/conditions" + id + ".json";
+    console.log(link);
+    // $.ajax({
+    //   url: link,
+    //   dataType: 'json',
+    //   jsonp: 'cb',
+    //   data: {
+    //     format: 'json'
+    //   },
+    //   success: handlers.processSearch
+    // });
+    $.getJSON(link, handlers.processSearch);
   },
 
-  processResults: function(data) {
-    // availableTags = [];
-    var table = $('#places').find('tbody').empty();
-    var i;
-    for (i in data.RESULTS) {
-      var place = data.RESULTS[i];
-      var tr = $('<tr>').appendTo(table);
-      $('<td>').text(place.name).appendTo(tr);
-      // availableTags.push(place.name);
-    }
+  processSearch: function(data) {
+    console.log(data.current_observation.display_location);
   }
 
 };
